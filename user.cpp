18e7249c3c78def5dde3f1f69096c5d63c52cc86
michpolak <124084861+michpolak@users.noreply.github.com>
2023-04-01 12:51:09 -0400

    user.cpp done (for now)

--- user.cpp
+++ user.cpp
@@ -7,4 +7,17 @@
 #include "user.h" 
 using namespace std; 
 
+user::user(){
+  username = "";
+  email = "";
+  password = "";
+  color = "";
+  
+}
 
+user::user(string username, string email, string password, string color){
+  this->username = username; 
+  this->email = email;
+  this->password = password;
+  this->color = color; 
+}
