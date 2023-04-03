1c9a2f96a5878dd6a6d4b0fa5abd4d2c5ed6f14d
michpolak <124084861+michpolak@users.noreply.github.com>
2023-04-03 15:32:26 -0400

    added set + get active

--- user.cpp
+++ user.cpp
@@ -31,3 +31,11 @@ string user::getEmail(){
 string user::getPassword(){
   return password;
 }
+
+void user::setActive(bool setActive){
+  active = setActive;
+}
+
+bool user::getActive(){
+  return active;
+}
