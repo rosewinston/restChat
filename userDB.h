cc21b148167a4cfd1b0f2eedb388adc8d1dde065
rosewinston <124084922+rosewinston@users.noreply.github.com>
2023-04-17 15:30:47 -0400

    userEntry correction

--- userDB.h
+++ userDB.h
@@ -38,7 +38,7 @@ public:
     vector<contactEntry> findByFirst(string first);
     vector<contactEntry> findByLast(string last);
     vector<contactEntry> findByType(string type);
-    contactEntry fetchEntry(string token);
+    userEntry fetchEntry(string token);
     void addEntry(string user,string email,string password,string color,bool active);
     void editEntry(string token,string user,string email,string password,string color,bool active);
     void deleteEntry(string idnum);
