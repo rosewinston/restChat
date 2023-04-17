341abb98d5963d58fa5f66b9ca2b0a0d1ff4f0d3
rosewinston <124084922+rosewinston@users.noreply.github.com>
2023-04-17 15:50:41 -0400

    changed it back to SQLString for json use

--- userEntry.h
+++ userEntry.h
@@ -9,7 +9,7 @@ class userEntry {
 public:
      userEntry();
   // CHECK THE LINE BELOW AGAIN
-     userEntry(sql::SQLString u, sql::SQLString e, sql::SQLString p, sql::SQLString c,sql::SQLBoolean a, sql::SQLString t);
+     userEntry(sql::SQLString u, sql::SQLString e, sql::SQLString p, sql::SQLString c,sql::SQLString a, sql::SQLString t);
      string text();
      string json();
      
@@ -17,8 +17,8 @@ public:
      string email;
      string password;
      string color;
-     boolean active;
-     string token; // WOULD THIS BE A STRING?
+     string active;
+     string token; // WOULD THIS BE A STRING? yes!
 
 private:
 
