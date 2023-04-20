1a49001bd102656fb8abba57e0634bbbc8e81452
michpolak <124084861+michpolak@users.noreply.github.com>
2023-04-20 16:37:26 -0400

    changed usrDB to userDB ???

--- restChat.cpp
+++ restChat.cpp
@@ -96,7 +96,7 @@ void addUser(string username, string email, string password, string color, map<s
 	user newUser(username, email, password, color, false);
 	
 	//trying userDB::addEntry (see userDB.cpp) below
-	usrDB.addEntry(username, email, password, color, "false");
+	userDB.addEntry(username, email, password, color, "false");
 	
         cout << "user created" << endl;
 	userMap[username]=newUser;
