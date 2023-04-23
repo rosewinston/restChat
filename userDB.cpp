b33c03420071a4e89132bbffbf773879c2250603
michpolak <124084861+michpolak@users.noreply.github.com>
2023-04-23 19:12:39 -0400

    thing

--- userDB.cpp
+++ userDB.cpp
@@ -138,6 +138,7 @@ string userDB::checkEmail(string email) {
     sql::ResultSet *res = stmnt->executeQuery("SELECT COUNT(1) FROM users WHERE Email = '%"+email+"%'");	 
     while (res->next()) {
     	status = res->getString("Count(1)");
+	cout<<status;
     }
     return status;	
 }
