559acbb329700428e1093b35df6a0f0fe9ddbab0
michpolak <124084861+michpolak@users.noreply.github.com>
2023-04-23 19:17:29 -0400

    caps? or cap

--- userDB.cpp
+++ userDB.cpp
@@ -137,7 +137,7 @@ string userDB::checkEmail(string email) {
     // Execute query
     sql::ResultSet *res = stmnt->executeQuery("SELECT COUNT(1) FROM users WHERE Email = '%"+email+"%'");	 
     while (res->next()) {
-    	status = res->getString("Count(1)");
+    	status = res->getString("COUNT(1)");
 
     }
     return status;	
